9e499c406f53932ea7fbdd8b6d278b591eaab55f
Rajesh <mail2rajeshgundu@gmail.com>
2025-03-06 19:02:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -7,21 +7,19 @@ using namespace std;
 
 class Solution {
   public:
-    int solve(string &s1, string &s2,int i,int j,vector<vector<int>>& dp) {
-        if(i >= s1.length() || j >= s2.length())
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(s1[i] == s2[j])
-            return dp[i][j] = 1+solve(s1,s2,i+1,j+1,dp);
-        return dp[i][j] = max(solve(s1,s2,i,j+1,dp),solve(s1,s2,i+1,j,dp));
-    }
     int lcs(string &s1, string &s2) {
-        // code here
         int n = s1.length();
         int m = s2.length();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return solve(s1,s2,0,0,dp);
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(s1[i-1] == s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp[n][m];
     }
 };
 
