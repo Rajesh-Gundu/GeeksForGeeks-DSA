25e0b31f3c81354c4c916edd1f7d4d188753ddab
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-19 16:55:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,23 +1,18 @@
 class Solution {
     int memoi(int n,vector<int>& height,vector<int>& dp) {
-        if(n < 0) return 0;
+        if(n <= 0) return 0;
+        if(n == 1)  return abs(height[1]-height[0]);
         if(dp[n] != -1) return dp[n];
         
-        int jumpOneStep = abs(height[n]-height[n-1]) + memoi(n-1,height,dp);
-        int jumpTwoSteps = INT_MAX;
-        if(n > 1)
-            jumpTwoSteps = abs(height[n]-height[n-2]) + memoi(n-2,height,dp);
-        
-        return dp[n] = min(jumpOneStep,jumpTwoSteps);
+        int one = memoi(n-1,height,dp)+abs(height[n]-height[n-1]);
+        int two = memoi(n-2,height,dp)+abs(height[n]-height[n-2]);
+        return dp[n] = min(one,two);
     }
-    
   public:
     int minCost(vector<int>& height) {
         // Code here
         int n = height.size();
-        vector<int> dp(n,-1);
-        dp[0] = 0;
-        memoi(n-1,height,dp);
-        return dp[n-1];
+        vector<int> dp(n+1,-1);
+        return memoi(n-1,height,dp);
     }
 };
