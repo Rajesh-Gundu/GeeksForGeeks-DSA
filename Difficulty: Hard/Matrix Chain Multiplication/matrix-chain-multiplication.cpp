5aab557e0ad7166f4c2857e18930338f1eac64d1
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-25 16:02:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -1,56 +1,19 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-    int solve(vector<int> &arr,int i,int j,vector<vector<int>>& dp) {
-        if(i+1 == j)
-            return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+    int solve(int i,int j,vector<int>& arr,vector<vector<int>>& dp) {
+        if(i == j)  return 0;
+        if(dp[i][j] != -1)  return dp[i][j];
         
-        int res = INT_MAX;
-        for(int k=i+1;k<j;k++) {
-            int cur = solve(arr,i,k,dp) + solve(arr,k,j,dp) + arr[i]*arr[k]*arr[j];
-            res = min(res,cur);
+        int mini = 1e9;
+        for(int k=i;k<j;k++) {
+            int cur = solve(i,k,arr,dp) + solve(k+1,j,arr,dp) + arr[i]*arr[k+1]*arr[j+1];
+            mini = min(mini,cur);
         }
-        
-        return dp[i][j] = res;
+        return dp[i][j] = mini;
     }
   public:
     int matrixMultiplication(vector<int> &arr) {
         int n = arr.size();
         vector<vector<int>> dp(n,vector<int> (n,-1));
-        return solve(arr,0,n-1,dp);
+        return solve(0,n-2,arr,dp);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // to ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.matrixMultiplication(a) << endl << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
